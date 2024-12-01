a1bb0cc32ab88fc6a6fdce210dd7fe1f96a1e177
Cole T. <coledt54@gmail.com>
2024-11-30 22:21:37 -0700

    resetting this so that I can actually learn how to neovim

--- init.lua
+++ init.lua
@@ -1,9 +1,1 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
--- "(„• ᴗ •„)"
-
-require("config.lazy")
-local lspconfig = require("lspconfig")
-lspconfig.basedpyright.setup({})
-
-vim.cmd("colorscheme tokyonight-night")
 vim.cmd("noremap <M-$> :!")
