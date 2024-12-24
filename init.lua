a1f505af3381dac2b5090d1cca4ac60851db78c3
Cole T. <coledt54@gmail.com>
2024-12-24 01:56:33 -0700

    nvim update

--- init.lua
+++ init.lua
@@ -1,1 +1,15 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+
 vim.cmd("noremap <M-$> :!")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("highlight Normal guibg=None")
+
+vim.call('plug#begin')
+
+Plug('xiyaowong/transparent.nvim')
+
+vim.call('plug#end')
+
